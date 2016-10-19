49037a482eaec881524187191702d0965f946b17
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2016-10-19 15:54:36 +0100

    More merge issue fixing

--- client/src/components/IconComponent.jsx
+++ client/src/components/IconComponent.jsx
@@ -5,14 +5,11 @@ var Icon = function(props){
     props.onClicked(props.id)
   }
   return(
-<<<<<<< HEAD
 
     <div>
       <img src={props.image} onClick={handleClick}></img>
     </div>
-=======
-      <img src={props.image}></img>
->>>>>>> f8427506ac7a231cb13d762b5f1858e2934b0d20
+
     )
 }
 
