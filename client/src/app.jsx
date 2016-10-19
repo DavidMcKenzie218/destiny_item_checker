f30b9bcf6b857221500e37c29740bf14aef8c0d9
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2016-10-19 13:22:36 +0100

    Initial Commit

---
+++ client/src/app.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+window.onload = function(){
+  ReactDOM.render(
+    <h3>Destiny Weapons</h3>,
+    document.getElementById('app')
+  );
+}
