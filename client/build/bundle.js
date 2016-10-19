58f1c3a05dc442e830869c29caee33487691f673
srobertson90 <stuart.robertson.applications@gmail.com>
2016-10-19 15:51:24 +0100

    dumping changes for merge

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -19759,23 +19759,45 @@
 	var InfoComponent = __webpack_require__(161);
 	var IconContainer = __webpack_require__(162);
 	
-	var testData = [{ name: "Weapon 1", description: "Weapon 1 descriptions", quote: "Weapon 1 quote", img: "http://www.bungie.net//common/destiny_content/icons/d88139c9e99fd5ec8d3beb7cf44938f7.jpg" }, { name: "Weapon 2", description: "Weapon 2 descriptions", quote: "Weapon 2 quote", img: "https://www.bungie.net/common/destiny_content/icons/a0a61a73bc5d680844824b795c14e7c9.jpg" }];
+	// var testData = [{name: "Weapon 1", description: "Weapon 1 descriptions", quote: "Weapon 1 quote", img: "http://www.bungie.net//common/destiny_content/icons/d88139c9e99fd5ec8d3beb7cf44938f7.jpg"}, {name: "Weapon 2", description: "Weapon 2 descriptions", quote: "Weapon 2 quote", img: "https://www.bungie.net/common/destiny_content/icons/a0a61a73bc5d680844824b795c14e7c9.jpg"}]
 	
 	var WeaponContainer = React.createClass({
 	  displayName: 'WeaponContainer',
 	
 	
 	  getInitialState: function getInitialState() {
-	    return { data: testData, selectedWeapon: { name: "Select a weapon", data: { desctiption: "No item selected", quote: "No item selected" } } };
+	    return { items: [], selectedItem: null };
+	  },
+	
+	  componentDidMount: function componentDidMount() {
+	    var url = "http://localhost:3000/api/items";
+	    var request = new XMLHttpRequest();
+	    request.open("GET", url);
+	    request.onload = function () {
+	      console.log("request has loaded");
+	      var jsonString = request.responseText;
+	      var data = JSON.parse(jsonString);
+	      this.setState({
+	        items: data
+	      });
+	    }.bind(this);
+	    request.send();
+	  },
+	
+	  setFocusCountry: function setFocusCountry(index) {
+	    var newItem = this.state.items[index];
+	    this.setState({
+	      focusItem: newItem
+	    });
 	  },
 	
 	  render: function render() {
 	    return React.createElement(
 	      'div',
 	      null,
-	      React.createElement(TitleComponent, { title: this.state.selectedWeapon.name }),
-	      React.createElement(IconContainer, { data: this.state.data }),
-	      React.createElement(InfoComponent, { name: this.state.selectedWeapon.name, data: this.state.selectedWeapon.data })
+	      React.createElement(TitleComponent, { title: this.state.selectedItem.name }),
+	      React.createElement(IconContainer, { data: this.state.item }),
+	      React.createElement(InfoComponent, { name: this.state.selectedItem.name, data: this.state.selectedItem })
 	    );
 	  }
 	
--- client/src/components/WeaponContainer.jsx
+++ client/src/components/WeaponContainer.jsx
@@ -3,21 +3,42 @@ var TitleComponent = require('./TitleComponent.jsx');
 var InfoComponent = require('./InfoComponent.jsx');
 var IconContainer = require('./IconContainer.jsx');
 
-var testData = [{name: "Weapon 1", description: "Weapon 1 descriptions", quote: "Weapon 1 quote", img: "http://www.bungie.net//common/destiny_content/icons/d88139c9e99fd5ec8d3beb7cf44938f7.jpg"}, {name: "Weapon 2", description: "Weapon 2 descriptions", quote: "Weapon 2 quote", img: "https://www.bungie.net/common/destiny_content/icons/a0a61a73bc5d680844824b795c14e7c9.jpg"}]
+// var testData = [{name: "Weapon 1", description: "Weapon 1 descriptions", quote: "Weapon 1 quote", img: "http://www.bungie.net//common/destiny_content/icons/d88139c9e99fd5ec8d3beb7cf44938f7.jpg"}, {name: "Weapon 2", description: "Weapon 2 descriptions", quote: "Weapon 2 quote", img: "https://www.bungie.net/common/destiny_content/icons/a0a61a73bc5d680844824b795c14e7c9.jpg"}]
 
 var WeaponContainer = React.createClass({
 
   getInitialState: function(){
-    return{data: testData, selectedWeapon: {name: "Select a weapon", data: {desctiption: "No item selected", quote: "No item selected"}}}
+    return{items: [], selectedItem: null}
   },
 
+  componentDidMount: function(){
+    var url = "http://localhost:3000/api/items";
+    var request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.onload = function(){
+      console.log("request has loaded");
+      var jsonString = request.responseText;
+      var data = JSON.parse(jsonString);
+      this.setState({
+        items: data
+      });
+    }.bind(this);
+    request.send();
+  },
+
+  setFocusCountry: function(index){
+    var newItem = this.state.items[index];
+    this.setState({
+      focusItem: newItem
+    });
+  },
 
   render: function(){
     return(
       <div>
-        <TitleComponent title={this.state.selectedWeapon.name}/>
-        <IconContainer data={this.state.data}/>
-        <InfoComponent name={this.state.selectedWeapon.name} data={this.state.selectedWeapon.data}/>
+        <TitleComponent title={this.state.selectedItem.name}/>
+        <IconContainer data={this.state.item}/>
+        <InfoComponent name={this.state.selectedItem.name} data={this.state.selectedItem}/>
       </div>
       )
   }
